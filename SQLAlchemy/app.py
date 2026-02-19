22a6a15e38426e99517ee318ba2f7e0928eb1ccf
Sundhari Hanish <sundharihanish@gmail.com>
2026-02-19 21:37:59 +0530

    Update app.py

--- SQLAlchemy/app.py
+++ SQLAlchemy/app.py
@@ -1,3 +1,4 @@
+# Flask CRUD application using SQLAlchemy to manage student records with SQLite database.
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 # Create Flask app
