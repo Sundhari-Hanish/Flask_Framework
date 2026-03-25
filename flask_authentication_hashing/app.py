debf228a569e522284682e958db0374e6f987e4e
Sundhari Hanish <sundharihanish@gmail.com>
2026-03-25 10:28:31 +0530

    Create app.py

---
+++ flask_authentication_hashing/app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_login import LoginManager, login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from config import Config
+from models import db, User
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+# Flask-Login setup
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/")
+def home():
+    return redirect(url_for("login"))
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+        new_user = User(
+            username=form.username.data,
+            password=hashed_password
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Registration successful! Please login.", "success")
+        return redirect(url_for("login"))
+
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash("Login successful!", "success")
+            return redirect(url_for("dashboard"))
+        else:
+            flash("Invalid username or password", "danger")
+    return render_template("login.html", form=form)
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Logged out successfully", "info")
+    return redirect(url_for("login"))
+
+@app.route("/users")
+@login_required
+def users():
+    all_users = User.query.all() 
+    return render_template("users.html", users=all_users)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  
+    app.run(debug=True)
