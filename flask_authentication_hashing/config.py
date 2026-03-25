4bcf58b33fcbad207338c15ebfa80405409bbc1a
Sundhari Hanish <sundharihanish@gmail.com>
2026-03-25 10:30:51 +0530

    Create config.py

---
+++ flask_authentication_hashing/config.py
@@ -0,0 +1,5 @@
+
+class Config:
+    SECRET_KEY = "your_secret_key"
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:Your_password@localhost/flask_db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
